eab8226c3574e1266867b27b42c05a30a2f087f1
Muzzamil Mushtaq <muzzamilmushtaq1@gmail.com>
2022-03-29 06:54:24 +0500

    ignore typescript error during build

--- 3.Kickstart/next.config.js
+++ 3.Kickstart/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // We are adding because vercel is trying to find  typescript types for ethereum folder as well 
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
